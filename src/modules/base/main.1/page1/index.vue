a469c459891ef3f268a316b4db4047f98e022536
huangwutao <346792184@qq.com>
2018-11-19 14:42:04 +0800

    新建

---
+++ src/modules/base/main.1/page1/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>{{msg}}</div>
+    <div>{{msg2}}</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: Global.host,
+      msg2: ENV.sit.mpaas_domainName
+    }
+  },
+  mounted() {
+    console.log(Global, ENV)
+  },
+}
+</script>
